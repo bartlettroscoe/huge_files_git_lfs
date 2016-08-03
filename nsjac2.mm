0ab402c9c2ff51ffbd6e6d77fb21746ee7f3800f
Roscoe A. Bartlett <rabartl@sandia.gov>
2016-08-02 21:20:23 -0600

    Small change

--- nsjac2.mm
+++ nsjac2.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4eeebbe773435114e1cd70fb84c9c93bf1b68574cb251ce6f114b450f9e4a48
+oid sha256:29c9ccfe994b63d8cc4f2a9ceee314d7f601399da99e4466d3aabf210c7ba069
 size 13573555
